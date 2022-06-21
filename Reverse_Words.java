729faeb143bc9ec1a1d9a59385f42fd46429d507
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-06-21 13:14:26 -0400

    Add files via upload

---
+++ Reverse_Words.java
@@ -0,0 +1,20 @@
+// Program which will reverse a String that is greater than or equal 
+// to 5 chars in length. Utilizes the StringBuilder method for reverse
+
+public class Reverse_Words {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(spinWords("I'm the absolute best throughout the land!"));
+		
+	}
+	public static String spinWords(String sentence) {
+	    String[] words = sentence.split(" ");
+	    for (int i=0; i<words.length; i++) {
+	      if (words[i].length() >= 5) {
+	        words[i] = new StringBuilder(words[i]).reverse().toString();
+	      }
+	    }
+	    return String.join(" ",words);
+	  }
+	}
